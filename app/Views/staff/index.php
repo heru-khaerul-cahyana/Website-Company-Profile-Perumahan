bcc7e4e9bffb2929e1cafd736e601ac326d40e76
heru-khaerul-cahyana <a2.1900080@mhs.stmik-sumedang.ac.id>
2023-09-18 02:35:41 +0700

    framework codeigniter

---
+++ app/Views/staff/index.php
@@ -0,0 +1,38 @@
+<main id="main">
+
+  <!-- ======= Breadcrumbs Section ======= -->
+  <section class="breadcrumbs">
+    <div class="container">
+
+      <div class="d-flex justify-content-between align-items-center">
+        <h2><?= $title ?></h2>
+        <ol>
+          <li><a href="<?= base_url() ?>">Home</a></li>
+          <li><?= $title ?></li>
+        </ol>
+      </div>
+
+    </div>
+  </section><!-- End Breadcrumbs Section -->
+
+  <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2><?= $title ?></h2>
+
+        </div>
+
+      </div>
+    <center>
+      <div class="image"> 
+          <img src="<?= base_url() ?>/assets/upload/image/thumbs/STRUKTUR ORGANISASI PERUSAHAAN.png" alt="User Image" width="1000px" >
+        </div>
+          <div class="col-lg-6">
+        </div>
+      </div>
+    </center>
+    </section><!-- End Contact Section -->
+
+</main><!-- End #main -->
